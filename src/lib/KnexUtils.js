26968e25c993be944b1f2ee8344e9fbccc6386d3
parul-smpx <parul.goyal@smartprix.com>
2017-09-18 19:28:04 +0530

    resetPgSequences function added in seeds

--- src/lib/KnexUtils.js
+++ src/lib/KnexUtils.js
@@ -28,14 +28,19 @@ async function recreateDb(env) {
 	// and then create the database using raw queries
 	let knex = Knex(dbConfig);
 	if (dbConfig.client === 'pg') {
-		// postgres doesn't allow dropping database while other user are connected
-		// so force other users to disconnect
-		await knex.raw(`ALTER DATABASE ${dbName} CONNECTION LIMIT 1`);
-		await knex.raw(`
-			SELECT pg_terminate_backend(pid)
-			FROM pg_stat_activity
-			WHERE datname = '${dbName}'
-		`);
+		try {
+			// postgres doesn't allow dropping database while other user are connected
+			// so force other users to disconnect
+			await knex.raw(`ALTER DATABASE ${dbName} CONNECTION LIMIT 1`);
+			await knex.raw(`
+				SELECT pg_terminate_backend(pid)
+				FROM pg_stat_activity
+				WHERE datname = '${dbName}'
+			`);
+		}
+		catch (e) {
+			// Ignore errors
+		}
 	}
 	await knex.raw(`DROP DATABASE IF EXISTS ${dbName}`);
 	await knex.raw(`CREATE DATABASE ${dbName}`);
