d96e8433b8d144d8967078986ce1d43878d4a197
Rohit Gohri <rohit.gohri@smartprix.com>
2018-10-26 14:08:55 +0530

    add types file

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+import Knex from 'knex'
+
+declare module '@smpx/knex-utils' {
+	function getKnexFile(): object;
+	function getKnex(); Knex;
+	function setKnex(knex: Knex): void;
+	function setLogger(logger: Console): void;
+	function dropDb(env: string): Promise<void>;
+	function createDb(env: string, opts?: {migrate?: boolean}): Promise<void>;
+	function recreateDb(env: string): Knex;
+	function refreshDb(env: string): Knex;
+	function resetPgSequences(): Promise<void>;
+	function seedFolder(folderPath: string): Promise<void>;
+	function addColumn(opts: {
+		table: string,
+		column: string, 
+		type: string,
+		default: any,
+		update: any,
+		updateInBatch?: boolean,
+		index?: boolean,
+		indexConcurrent?: boolean,
+	}): Promise<void>;
+	function updateColumnInBatch(opts: {table: string, column: string, update: any}): Promise<void>;
+}
